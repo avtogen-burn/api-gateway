5e1c617d1abbaea13ab06c64ebfe908f65e57eb5
Anton Shchekochikhin <anton.shchekochihin@yandex.ru>
2024-09-13 15:45:57 +0300

    Boilerplate

---
+++ src/main/java/ru/avt/api_gateway/web/controller/HelloController.java
@@ -0,0 +1,13 @@
+package ru.avt.api_gateway.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello root";
+    }
+}
